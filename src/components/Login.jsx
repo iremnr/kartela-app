a4e6917bf55f751c0202bc423a0a433e6cc14378
irem <mermer.iremnur@gmail.com>
2023-02-02 00:22:05 +0300

    menu pages modified

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -7,43 +7,46 @@ import kartela_logo from '../assets/logo_kartela.png';
 function Login() {
     return (
         
-        <Container className="app">
-            <Row className="row-body">
-                <Col sm={6}>
+        <Container fluid className="app">
+            <Row className="row-out">
+                <Row className="row-body">
+                    <Col sm={6}>
 
-                    <div className="row-1">
-                        <h2 className="giris-control">GİRİŞ</h2>
-                    </div>
-                    
-                    <div className="row-1">
-                        <form className="form">
-                            <div class="form-floating mb-3">
-                                <input type="text" class="form-control" id="floatingInput" placeholder="Kullanıcı Adı"/>
-                                <label for="floatingInput">Kullanıcı Adı</label>
-                            </div>
+                        <div className="row-1">
+                            <h2 className="giris-control">GİRİŞ</h2>
+                        </div>
+                        
+                        <div className="row-1">
+                            <form className="form">
+                                <div class="form-floating mb-3">
+                                    <input type="text" class="form-control" id="floatingInput" placeholder="Kullanıcı Adı"/>
+                                    <label for="floatingInput">Kullanıcı Adı</label>
+                                </div>
 
-                            <div class="form-floating mb-3">
-                                <input type="password" class="form-control" id="floatingInput" placeholder="Şifre"/>
-                                <label for="floatingInput">Şifre</label>
-                            </div>
+                                <div class="form-floating mb-3">
+                                    <input type="password" class="form-control" id="floatingInput" placeholder="Şifre"/>
+                                    <label for="floatingInput">Şifre</label>
+                                </div>
 
-                        </form>
-                    </div>
+                            </form>
+                        </div>
 
-                    <div className="">
-                        <button className="button">GİRİŞ</button>
-                    </div> 
+                        <div className="">
+                            <button className="button">GİRİŞ</button>
+                        </div> 
 
-                    <div className="kartela-logo">
-                        <img className="kartela-logo" src={kartela_logo} alt="" />
-                    </div> 
+                        <div className="kartela-logo">
+                            <img className="kartela-logo" src={kartela_logo} alt="" />
+                        </div> 
 
-                </Col>
+                    </Col>
 
-                <Col sm={6} className="login-img" >
-                    <img className="login-img" src={login_img} alt="" />
-                </Col>
+                    <Col sm={6} className="login-img" >
+                        <img className="login-img" src={login_img} alt="" />
+                    </Col>
+                </Row>
             </Row>
+
         </Container>
 
 
--- src/components/user_menu.jsx
+++ src/components/user_menu.jsx
@@ -6,39 +6,46 @@ import { BsArrowLeftCircle } from "react-icons/bs";
 
 function User_menu() {
     return (
-        <Container>
-            <Row>
-                <Col sm={4} className="vector" >
-                    <BsArrowLeftCircle size={50} color="#369CC0"/>
-                </Col>
-                <Col sm={8} className="" >
-                    <img className="" src={kartela_logo} alt="" />
-                </Col>
-            </Row>
+        <Container fluid className="app">
+            <Row className="row-out">
+                <Row className="row-body">
 
-            <Row>
-                <Col md={6}>
-                    <div className="Card-1 mb-8">
-                        <h1 className='card-yazi-1'>TAKVİM</h1>
-                    </div>
-                    <div className="Card-1 mb-8">
-                        <h1 className='card-yazi-1'>ETKİNLİK OLUŞTUR</h1>
-                    </div>
-                </Col>
+                    <Row>
+                        <Col sm={4} className="vector" >
+                            <BsArrowLeftCircle size={50} color="#369CC0"/>
+                        </Col>
+                        <Col sm={8} className="" >
+                            <img className="" src={kartela_logo} alt="" />
+                        </Col>
+                    </Row>
 
-                <Col md={6}>
-                    <div className="Card-1 mb-8" >
-                        <h1 className='card-yazi-1'>SEANS OLUŞTUR</h1>
-                    </div>
+                    <Row>
+                        <Col md={6}>
+                            
+                            <div className="Card-1 mb-8">
+                                <h1 className='card-yazi-1'>TAKVİM</h1>
+                            </div>
 
-                    <div className='Card-2 mb-8'>
-                        <h1 className='card-yazi-2'>SEANSLARIM</h1>
-                    </div>
-                    <div className='Card-2 mb-8'>
-                        <h1 className='card-yazi-2'>DANIŞANLARIM</h1>
-                    </div>
-                    
-                </Col>
+                            <div className="Card-1 mb-8">
+                                <h1 className='card-yazi-1'>ETKİNLİK OLUŞTUR</h1>
+                            </div>
+                        </Col>
+
+                        <Col md={6}>
+                            <div className="Card-1 mb-8" >
+                                <h1 className='card-yazi-1'>SEANS OLUŞTUR</h1>
+                            </div>
+
+                            <div className='Card-2 mb-8'>
+                                <h1 className='card-yazi-2'>SEANSLARIM</h1>
+                            </div>
+                            <div className='Card-2 mb-8'>
+                                <h1 className='card-yazi-2'>DANIŞANLARIM</h1>
+                            </div>
+                            
+                        </Col>
+                    </Row>
+                </Row>
             </Row>
         </Container>
     )
