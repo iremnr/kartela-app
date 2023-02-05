51193b53f9a92a1e5486fdcae6abd3fa8b67ba93
kadriyeg <denizk2407@gmail.com>
2023-02-05 19:13:56 +0300

    addroom page design updates

--- src/components/AddRoom.jsx
+++ src/components/AddRoom.jsx
@@ -2,7 +2,6 @@ import "@fontsource/roboto";
 import "@fontsource/montserrat";
 import "../App.css";
 import logo from "../logoKartela.svg";
-import figure from "../back-page.svg";
 import { useState } from "react";
 import classNames from 'classnames';
 import { Button, Col, Container, Row } from "react-bootstrap";
@@ -27,35 +26,40 @@ function AddRoom(){
   });  
 
  return (
-    <Container fluid>
-        <Row className="page-header"> 
-        <Col sm ={{ order: 'last' }} md={{ order: 'last' }} lg={{ order: 'last' }}><img src={logo} className="kartela-logo" alt="logo" /></Col>
+      <Container fluid className="page-container">
+      <Container fluid className="inner-container">
+      <Row className="page-header" xs={12} sm={12} md={12} lg={12} xl={12}>
         <Col className="prev-page"> <BsArrowLeftCircle size={50} color="#369CC0"/></Col>
-      </Row>
-      <Col className="room-body">
-        <Row className="add-session-title">ODA EKLE</Row>
-        <Row className="room-name">ODA İSMİ
-        <select value={room} onChange={(e) => setRoom(e.target.value)} placeholder="Oda seçiniz">
-                {rooms.map((room) => (
-                  <option value={room.key} key={room.key}>
-                    {room.value}
-                  </option>
-                ))}
-              </select>
-        </Row>
-        <Row className="color-box"></Row>
-        <Row className="form-title">RENK</Row>
+      <Col className="kartela-logo" lg={3}><img src={logo}  alt="logo" /></Col>
+    </Row>
+      <Row className="add-session-title">ODA EKLE</Row>
+     <Row className="deneme">
+     <Row className="room-form-box">
+            <select  className="form-box-input" value={room} onChange={(e) => setRoom(e.target.value)}>
+              {rooms.map((room) => (
+                <option value={room.key} key={room.key}>
+                  {room.value}
+                </option>
+              ))}
+            </select>
+        </Row></Row>
+     <Row className="deneme">
         <Row className="colors">
-          <Col><Button className="green-button" onClick={() => alert('Renk Kaydedildi')}></Button></Col>
-          <Col><Button className="red-button" onClick={() => alert('Renk Kaydedildi')}></Button></Col>
-          <Col><Button className="orange-button" onClick={() => alert('Renk Kaydedildi')}></Button></Col>
-          <Col><Button className="yellow-button" onClick={() => alert('Renk Kaydedildi')}></Button></Col>
-          <Col><Button className="purple-button" onClick={() => alert('Renk Kaydedildi')}></Button></Col>
-          <Col><Button className="pink-button" onClick={() => alert('Renk Kaydedildi')}></Button></Col>
+          <Button className="green-button" onClick={() => alert('Renk Kaydedildi')}></Button>
+          <Button className="red-button" onClick={() => alert('Renk Kaydedildi')}></Button>
+          <Button className="orange-button" onClick={() => alert('Renk Kaydedildi')}></Button>
+          <Button className="yellow-button" onClick={() => alert('Renk Kaydedildi')}></Button>
+          <Button className="purple-button" onClick={() => alert('Renk Kaydedildi')}></Button>
+          <Button className="pink-button" onClick={() => alert('Renk Kaydedildi')}></Button>
         </Row>
-        <Row><Button className="session-submit" onClick={() => alert('Oda Kaydedildi')}>KAYDET</Button></Row>
-      </Col>
-    </Container>
+       </Row>
+       <Row className="deneme">
+      <Button className="room-submit" onClick={() => alert('Oda Kaydedildi')}>KAYDET</Button>
+    
+    </Row>
+   
+       </Container>
+       </Container>
  )
 }
 
